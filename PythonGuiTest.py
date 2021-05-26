646b37296b0be16dc2ab9282c9f4e6876bcf1916
nandusreevalsan <55938590+nandusreevalsan@users.noreply.github.com>
2021-05-26 15:14:08 +0400

    Add files via upload

---
+++ PythonGuiTest.py
@@ -0,0 +1,25 @@
+# A simple python GUI example code.
+
+import PySimpleGUI as sg
+
+# Define the window's contents
+layout = [[sg.Text("What about your name?")],
+          [sg.Input()],
+          [sg.Button('Ok')],
+          [sg.Button('Podooo')]]
+
+#Create the window
+
+window = sg.Window("Chumma Test", layout)
+
+event, values = window.read()
+
+if event == 'Ok':
+    print('Hello', values[0], "Thanks for trying my code")
+else:
+    print("Ohoo than podoooooo")
+
+
+window.close()
+
+
