67516c9c12f43414b60a49f2b20a763b206b46d3
Nandu Sreevalsan <nandu.sreevalsan@gmail.com>
2021-06-03 13:25:50 +0400

    New update

--- PythonBMICalculator.py
+++ PythonBMICalculator.py
@@ -1,5 +1,4 @@
 # A simple python GUI example code.
-# Programmed by Nandu Sreevalsan.
 
 import PySimpleGUI as sg
 import time
@@ -9,7 +8,11 @@ layout = [[sg.Text("Enter your weight in kg")],
           [sg.Input(key="weight")],
           [sg.Text("Enter your height in cm")],
           [sg.Input(key="height")],
+          [sg.Text("Enter your required BMI")],
+          [sg.Input(key="ReqBMI")],
           [sg.Button('Ok')],
+          [sg.Button('Ideal weight')],
+          [sg.Text(size=(60,1),key='Weight')],
           [sg.Text(size=(40,1),key='OUTPUT')]]
 
 #Create the window
@@ -24,23 +27,33 @@ while Event == True:
     print(event)
     if event == 'Ok':
         try:
-            Wt = int(values['weight'])
-            Ht = int(values['height'])
+            Wt = float(values['weight'])
+            Ht = float(values['height'])
             BMI = Wt/(Ht*Ht)*10000
             values["BMI"] = str(BMI)
             print(values)
-            if(BMI>25):
-                window['OUTPUT'].update("Yo FATSOOOOO, your BMI is: "+values["BMI"])
-            elif(BMI<25):
-                window['OUTPUT'].update("Hmm, your BMI is: "+values["BMI"])
-
+            window['OUTPUT'].update("BMI is: "+values["BMI"])
         except:
             window['OUTPUT'].update("Please enter the correct values")
-    else:
-        print("Ohoo than podoooooo")
+    elif event == "Ideal weight":
+        try:
+            Ht = float(values['height'])
+            Wt = float(values['weight'])
+            ReqBMI = float(values['ReqBMI'])
+            ReqWt = ReqBMI * Ht*Ht/10000
+            Wtloss = Wt - ReqWt
+            values["ReqWt"] = str(ReqWt)
+            values["Wtloss"] = str(Wtloss)
+            window['Weight'].update("Req Wt is :"+ values["ReqWt"]+" You have to loose : " + values["Wtloss"])
+        except:
+            window['Weight'].update("Please enter the correct values")
+
+
+
     if event == None:
         Event = False
         break
+
 #test
 
 #window.close()
